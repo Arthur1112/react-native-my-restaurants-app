58e8957181afe064a2e3933d71c2fe1dd4cb62f3
Arthur <arthur_2335@hotmail.com>
2022-05-25 15:13:22 -0400

    connected restaurants

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
 
 export default function App() {
-  fetch('https://my-first-firestore-as.web.app/restaurants')
+  const [allRestaurants, setAllRestaurants] = useState()
+
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+      const response = await fetch('https://my-first-firestore-as.web.app/restaurants')
+      const data = await response.json()
+      setAllRestaurants(data)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    getData()
+  }, [])
 
   return (
     <View style={styles.container}>
+      {allRestaurants?.map((singleRest) => {
+        return <Text key={singleRest.id}>{singleRest.name}</Text>
+      })}
       <Text>Hey Class</Text>
       <StatusBar style="auto" />
     </View>
