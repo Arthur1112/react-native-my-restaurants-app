ae56193b767b6e23c02da7be60b01938f8277362
Arthur <arthur_2335@hotmail.com>
2022-05-26 14:09:38 -0400

    imported context to Home

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { ScrollView, ActivityIndicator, TouchableOpacity } from "react-native";
+import { SingleRestContext } from "../../App";
 import RestaurantCard from "../components/RestaurantCard";
 
 export default function Home({ navigation }){
     const [allRestaurants, setAllRestaurants] = useState();
+    const {setCurrentRest} = useContext(SingleRestContext)
+
     useEffect(() => {
         fetch('https://my-first-firestore-as.web.app/restaurants')
             .then(res  => res.json())
