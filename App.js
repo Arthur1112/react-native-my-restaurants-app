8b5602c226261dbad1c547ffd8fee6c9501d1d87
Arthur <arthur_2335@hotmail.com>
2022-05-26 13:20:18 -0400

    added title to home component

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/scenes/Home';
-import { SafeAreaView } from 'react-native';
 
 const bgImage = {uri: 'https://png.pngtree.com/background/20210709/original/pngtree-food-western-food-steak-tomato-picture-image_941801.jpg'}
 
@@ -12,7 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name='Home' component={Home} />
+          <Stack.Screen name='Home' component={Home} options={{title: 'Local Restaurants'}}/>
         </Stack.Navigator>
         <StatusBar style="auto" />
     </NavigationContainer>
