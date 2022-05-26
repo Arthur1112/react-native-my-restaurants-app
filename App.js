10bd4ac0f03d789f01ca6ecd00df17be441504a1
Arthur <arthur_2335@hotmail.com>
2022-05-26 14:00:10 -0400

    added context to app.js

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { createContext, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -6,16 +7,23 @@ import Details from './src/scenes/Details';
 
 const bgImage = {uri: 'https://png.pngtree.com/background/20210709/original/pngtree-food-western-food-steak-tomato-picture-image_941801.jpg'}
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
+
+export const SingleRestContext = createContext(null); // we usually use null for a default context
+
 
 export default function App() {
+  const [currentRest, setCurrentRest] = useState();
+
   return (
-    <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name='Home' component={Home} options={{title: 'Local Restaurants'}}/>
-          <Stack.Screen name='Details' component={Details} options={{title: 'Restaurant Details'}}/>
-        </Stack.Navigator>
-        <StatusBar style="auto" />
-    </NavigationContainer>
+    <SingleRestContext.Provider>
+      <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name='Home' component={Home} options={{title: 'Local Restaurants'}}/>
+            <Stack.Screen name='Details' component={Details} options={{title: 'Restaurant Details'}}/>
+          </Stack.Navigator>
+          <StatusBar style="auto" />
+      </NavigationContainer>
+    </SingleRestContext.Provider>
   );
 }
