9501e30a8d069b87dce7c692b7c67fef10450822
Arthur <arthur_2335@hotmail.com>
2022-05-26 11:41:25 -0400

    create Home.js

---
+++ src/scenes/Home.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { ScrollView, ActivityIndicator } from "react-native";
+import RestaurantCard from "../components/RestaurantCard";
+
+export default function Home(){
+    const [allRestaurants, setAllRestaurants] = useState();
+    useEffect(() => {
+        fetch('https://my-first-firestore-as.web.app/restaurants')
+            .then(res  => res.json())
+            .then(setAllRestaurants)
+            .catch(console.error)
+    }, [])
+    return (
+        <ScrollView>
+            {!allRestaurants
+                ?<ActivityIndicator size ='large' color='orange'/>
+                : allRestaurants.map(single => (
+                    <RestaurantCard key={singleRest.id} singleRest={singleRest} />
+                ))
+            }
+        </ScrollView>
+    )
+}
