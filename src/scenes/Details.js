eb03ee931ef4f6921ad2f2c0f3ca1ab7b89c99fa
Arthur <arthur_2335@hotmail.com>
2022-05-26 14:25:37 -0400

    added more text and styles to details.js

--- src/scenes/Details.js
+++ src/scenes/Details.js
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 import { View, Text } from "react-native";
+import { ActivityIndicator } from "react-native";
+import { Image } from "react-native-web";
 import { SingleRestContext } from "../../App";
 import styles from "../styles";
 
@@ -7,7 +9,18 @@ export default function Details () {
     const { currentRest } = useContext(SingleRestContext)
     return (
         <View style={styles.restaurantCard}>
+        {!currentRest
+        ? <ActivityIndicator />
+        : (
+            <>
+            <Image 
+            source={{uri: currentRest.image}} style={{width: '100%', height: 240}} />
             <Text style={styles.restaurantsName}>{currentRest.name}</Text>
+            <Text style={styles.cuisine}>{currentRest.cuisine}</Text>
+            <Text style={styles.address}>{currentRest.address}</Text>
+            <Text style={[styles.address, {fontWeight: '700'}]}>Rating {currentRest.rating}</Text>
+            </>
+        )}
         </View>
     )
 }
